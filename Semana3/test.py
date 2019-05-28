696a71fef815c37d6e2dca494ff221af0415c36f
juancagudelo <juanagudelo@seti.com.co>
2019-05-28 15:31:27 -0500

    Ajuste Pruebas Unitarias Todos los casos

--- Semana3/test.py
+++ Semana3/test.py
@@ -2,13 +2,20 @@
 import pytest
 import main
 
-@pytest.mark.parametrize("param1, param2",[(0, 0),(0, 1),(1, 1),(1, 0)])
-def test_compuertaOr(param1, param2):
+@pytest.mark.parametrize("param1, param2",[(0, 1),(1, 1),(1, 0)])
+def test_compuertaOrVerdadero(param1, param2):
     COMPUERTA_OR = main.CompuertaOr(param1, param2)
     assert COMPUERTA_OR.operacion() == 'VERDADERO'
 
-@pytest.mark.parametrize("param1, param2",[(0, 0),(0, 1),(1, 1),(1, 0)])
-def test_compuertaAnd(param1, param2):
+def test_compuertaOrFalso():
+    COMPUERTA_OR = main.CompuertaOr(0, 0)
+    assert COMPUERTA_OR.operacion() == 'FALSO'
+
+@pytest.mark.parametrize("param1, param2",[(0, 0),(0, 1),(1, 0)])
+def test_compuertaAndFalso(param1, param2):
     COMPUERTA_AND = main.CompuertaAnd(param1, param2)
     assert COMPUERTA_AND.operacion() == 'FALSO'
 
+def test_compuertaAndVerdadero():
+    COMPUERTA_AND = main.CompuertaAnd(1, 1)
+    assert COMPUERTA_AND.operacion() == 'VERDADERO'
